a3dd1fd818e5448b26314a7089d57dd011984098
Tuan Le <letuanlt09@gmail.com>
2020-05-10 21:47:16 -0400

    first commit

---
+++ server/config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  db: {
+    uri: 'mongodb+srv://guest:CEN3031access@cluster0-w71rg.mongodb.net/test?retryWrites=true&w=majority'//place the URI of your mongo database here.
+  }, 
+  openCage: {
+    key: '40b076536ccd41e39883a37fc3558cc5' //place your openCage public key here - Sign-up for a free key https://opencagedata.com/
+  },
+  port: 8080
+};
